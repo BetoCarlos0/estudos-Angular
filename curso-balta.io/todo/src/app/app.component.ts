98156037831c43d585f4539f0f9a9249f0f5bec3
Roberto Carlos <betocarlos00@hotmail.com>
2022-03-31 19:55:25 -0300

    final, começando com angular

--- curso-balta.io/todo/src/app/app.component.ts
+++ curso-balta.io/todo/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Todo } from './models/todo.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public mode: String = 'list';
   public todos: Todo[] = [];
   public title: string = 'minhas tarefas';
   public form: FormGroup;
@@ -20,21 +21,50 @@ export class AppComponent {
         Validators.required,
       ])]
     });
-    this.todos.push(new Todo(1, 'passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'ir ao supermercado', false));
-    this.todos.push(new Todo(3, 'cortar cabelo', true));
+
+    this.load();
   }
 
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear(){
+    this.form.reset();
+  }
+  
   remove(todo: Todo){
     const index = this.todos.indexOf(todo);
     if(index != -1){
       this.todos.splice(index, 1);
     }
+    this.save();
   }
   markAsDone(todo: Todo){
     todo.done = true;
+    this.save();
   }
   markAsUndone(todo: Todo){
     todo.done = false;
+    this.save();
+  }
+  
+  save(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+    this.mode = 'list';
+  }
+  
+  load(){
+    const data = localStorage.getItem('todos') || '[]';
+    this.todos = JSON.parse(data);
+  }
+
+  changeMode(mode: string){
+    this.mode = mode;
   }
 }
