70d9a7008111781e746bf075a4b56d2e4c0d2e50
Roberto Carlos <betocarlos00@hotmail.com>
2023-12-02 13:33:33 -0300

    comunicação com back-end

---
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent {
+  pensamento: Pensamento = {
+    id: 1,
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  criarPensamento() {
+    alert("Novo pensamento criado!")
+  }
+
+  cancelar() {
+    alert("Ação cancelada!")
+  }
+}
